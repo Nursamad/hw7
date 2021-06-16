1e20f23aa617dcc8b56d215dda7f0392ab79c7da
Nursamad <bagishbekow@gmail.com>
2021-06-16 11:41:03 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Hero implements HavingSuperAbility {
+    String name;
+    int hp;
+    int damage;
+
+
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+
+    }
+}
